4595edc21a423fa3e38cb395e6bd3a1a1dc5e538
Lucas Anderson de Souza Marques <lucasandersonmarques@gmail.com>
2023-05-31 15:33:01 -0300

    estilizando a sidebar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,5 @@
+import { FotoPerfil } from './styles'
+
+const Avatar = () => <FotoPerfil src="https://github.com/Lucass-marques.png" />
+
+export default Avatar
--- src/container/Sidebar/index.tsx
+++ src/container/Sidebar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Title from '../../components/Title'
+import { Description, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/Lucass-marques.png" />
-    <Title fontSize={20}>Lucas Marques</Title>
+    <SidebarContainer>
+      <Avatar />
+      <Title fontSize={20}>Lucas Marques</Title>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        Lucass-marques
+      </Paragrafo>
+      <Description tipo="principal" fontSize={12}>
+        Estudante Front-end
+      </Description>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
